0efdae4da3911ff2b7121f9ee67a786ad8039d67
Edinorock <111507622+Edinorock@users.noreply.github.com>
2022-11-20 13:54:54 +0300

    Задание 2.2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,21 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
 
-
-
-
+        Set<Integer> numbers = new HashSet<>();
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) {
+            numbers.add(random.nextInt(1000));
+        }
+        System.out.println(numbers);
+        Iterator<Integer> iterator = numbers.iterator();
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            if (next % 2 != 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println(numbers);
     }
 }
